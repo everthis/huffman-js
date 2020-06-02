89f2162213fd4ca6ba154c1f52e75b6978221d11
everthis <wwwaap@gmail.com>
2020-06-02 21:13:24 +0800

    init project.

---
+++ index.js
@@ -0,0 +1,13 @@
+function decode() {}
+function encode() {}
+function readHeader() {}
+function writeHeader() {}
+function readBit() {}
+function flushBuffer() {}
+function writeBit() {}
+function decodeBitStream() {}
+function encodeAlphabet() {}
+function buildTree() {}
+function addLeaves() {}
+function addNode() {}
+function determineFrequency() {}
