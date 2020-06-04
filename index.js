25d9c15ba5c69d35f6930825561959b5e2a2c70d
everthis <wwwaap@gmail.com>
2020-06-04 22:06:47 +0800

    Add frequency function.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+const { EOL } = os
+const alphabetsNum = 2 ** 8
+
 function decode() {}
 function encode() {}
 function readHeader() {}
@@ -10,4 +16,33 @@ function encodeAlphabet() {}
 function buildTree() {}
 function addLeaves() {}
 function addNode() {}
-function determineFrequency() {}
+function determineFrequency(file) {
+  return new Promise((resolve, reject) => {
+    const rs = fs.createReadStream(file, {
+      encoding: 'utf8',
+    })
+    const m = new Map()
+    let size = 0
+
+    rs.on('data', (chunk) => {
+      for (let e of chunk) {
+        if (e === EOL) continue
+        m.set(e, (m.get(e) || 0) + 1)
+        size++
+      }
+    })
+    rs.on('end', () => {
+      resolve({
+        map: m,
+        originalSize: size,
+        activeNum: m.size,
+      })
+    })
+    rs.on('error', (err) => {
+      reject(err.stack)
+    })
+  })
+}
+
+const testFile = path.join(__dirname, 'test.txt')
+determineFrequency(testFile).then((e) => console.log(e))
