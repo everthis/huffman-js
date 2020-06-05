86a2234317d6cee07d97f6530d6986d4593276f8
Jie He <jihe@conviva.com>
2020-06-05 17:23:15 +0800

    update functions

--- index.js
+++ index.js
@@ -12,10 +12,46 @@ function readBit() {}
 function flushBuffer() {}
 function writeBit() {}
 function decodeBitStream() {}
-function encodeAlphabet() {}
-function buildTree() {}
-function addLeaves() {}
-function addNode() {}
+function encodeAlphabet() {
+  let nodeIdx = leafIdx[character + 1], stackTop = 0
+  while(nodeIdx < nodesNum) {
+    stack[stackTop++] = nodeIdx % 2
+    nodeIdx = parentIdx[Math.floor((nodeIdx + 1) / 2)]
+  }
+  while(--stackTop > -1) writeBit(fout, stack[stackTop])
+}
+function buildTree() {
+  let a, b, idx
+  while(freeIdx < nodesNum) {
+    a = freeIdx++
+    b = freeIdx++
+    idx = addNode( b >> 1, nodes[a].weight + nodes[b].weight )
+    parentIdx[b >> 1] = idx
+  }
+}
+function addLeaves() {
+  for(let i = 0, freq = 0; i < alphabetsNum; i++) {
+    freq = frequency[i]
+    if(freq > 0) addNode(-(i + 1), freq)
+  }
+}
+function addNode(idx, weight) {
+  let i = num_nodes++
+  while(i > 0 && nodes[i].weight > weight) {
+    if(nodes[i].idx < 0) {
+      ++leafIdx[-nodes[i].idx]
+    } else {
+      ++parentIdx[nodes[i].idx]
+    }
+    --i
+  }
+  ++i
+  nodes[i].idx = idx
+  nodes[i].weight = weight
+  if(idx < 0) leafIdx[-idx] = i
+  else parentIdx[idx] = i
+  return i
+}
 function determineFrequency(file) {
   return new Promise((resolve, reject) => {
     const rs = fs.createReadStream(file, {
@@ -23,10 +59,8 @@ function determineFrequency(file) {
     })
     const m = new Map()
     let size = 0
-
     rs.on('data', (chunk) => {
       for (let e of chunk) {
-        if (e === EOL) continue
         m.set(e, (m.get(e) || 0) + 1)
         size++
       }
